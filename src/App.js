850d4ea570724c2b19eccc9ae1ef23a14c29ec3f
Amit <your_email_address@example.com>
2022-01-05 17:58:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import '/node_modules/bootstrap/dist/css/bootstrap.rtl.min.css'
+
+function App() {
+  return (
+    <>
+     <div>
+       <h1>React chander</h1>
+     </div>
+    </>
+  );
+}
+
+export default App;
